56862bcb1c49f6d2c0190d90b783ac171ac98dc5
Jack Cooper <jackcooper1245@gmail.com>
2020-04-17 12:10:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   def self.find_or_create_by_name(song_name)
      if
        self.find_by_name(song_name)
-       return self
+       return Song
     else
       self.create_by_name(song_name)
     end
