5e4c4ac3994512a8f8c00604a5aa6203e87596d2
Jack Cooper <jackcooper1245@gmail.com>
2020-04-17 12:04:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,7 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    if @@all.find{|song| song.song_name == song_name}
-  else
-    self.new
-  end
+    @@all.find{|song| song.song_name == song_name}
   end
 
 end
